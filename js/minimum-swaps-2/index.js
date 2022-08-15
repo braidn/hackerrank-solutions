afbe8362e8fad47a0555a8aa6abe3520bc15d670
Braden Douglass <braden.douglass@gmail.com>
2022-08-15 16:25:14 -0400

    Working Version Using IndexOf Array Method.
    Get a working version that seems to work locally but is failing some
    Hackerrank tests. Larger arrays are likely slowing this down.

--- js/minimum-swaps-2/index.js
+++ js/minimum-swaps-2/index.js
@@ -1,3 +1,20 @@
 export function minimumSwaps(arr) {
-  return arr.length
+  const maxLength = arr.length;
+  let swaps = 0;
+
+  for (let i = 0; i < maxLength; i++) {
+    const currentItem = arr[i];
+    const targetItem = i + 1;
+
+    if (currentItem !== targetItem) {
+      const indexTarget = arr.indexOf(targetItem);
+
+      arr[indexTarget] = currentItem;
+      arr[i] = targetItem;
+      swaps++;
+      console.log(arr)
+    }
+  }
+
+  return swaps;
 }
--- js/minimum-swaps-2/test.js
+++ js/minimum-swaps-2/test.js
@@ -2,7 +2,12 @@ import test from 'node:test'
 import assert from 'assert/strict'
 import { minimumSwaps } from './index.js'
 
-test('returns the minimum swap needed to sort the array', () => {
+test('returns an integer that represents the minimum swap needed to sort the array', () => {
   const arr = [4, 3, 1, 2]
   assert.equal(minimumSwaps(arr), 3)
+})
+
+test('returns an integer that represents the minimum swap needed to sort the array', () => {
+  const arr = [7, 1, 3, 2, 4, 5, 6]
+  assert.equal(minimumSwaps(arr), 5)
 })
